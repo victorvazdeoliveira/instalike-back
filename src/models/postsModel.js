c32c0cde5cb7a1bf5e229f9ed4225683eb745172
Victor Vaz de Oliveira <contato@victorvazdeoliveira.com.br>
2024-11-21 14:18:30 -0300

    Arquivos desenvolvidos na Aula 4

--- src/models/postsModel.js
+++ src/models/postsModel.js
@@ -8,3 +8,10 @@ export async function getTodosPosts() {
 
     return colecao.find().toArray();
 }
+
+export async function criarPost(novoPost) {
+    const db = conexao.db("instalike");
+    const colecao = db.collection("posts");
+
+    return colecao.insertOne(novoPost);
+}
