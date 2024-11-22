4c03e1cd6bd2b4a646159733d6864b6deb2fdb9c
Victor Vaz de Oliveira <contato@victorvazdeoliveira.com.br>
2024-11-22 14:20:17 -0300

    Projeto localhost finalizado

--- server.js
+++ server.js
@@ -2,7 +2,8 @@ import express from "express";
 import routes from "./src/routes/postsRoutes.js";
 
 const app = express();
-routes(app)
+app.use(express.static("uploads"));
+routes(app);
 
 app.listen(3000, () => { // O nome da função de retorno é Arrow function.
     console.log("Servidor escutando...");
--- src/models/postsModel.js
+++ src/models/postsModel.js
@@ -1,3 +1,4 @@
+import { ObjectId } from "mongodb";
 import conectarAoBanco from "../config/dbConfig.js";
 
 const conexao = await conectarAoBanco(process.env.STRING_CONEXAO);
@@ -14,4 +15,12 @@ export async function criarPost(novoPost) {
     const colecao = db.collection("posts");
 
     return colecao.insertOne(novoPost);
+}
+
+export async function atualizarPost(id, novoPost) {
+    const db = conexao.db("instalike");
+    const colecao = db.collection("posts");
+    const objectId = ObjectId.createFromHexString(id);
+
+    return colecao.updateOne({_id: new ObjectId(objectId)}, {$set:novoPost});
 }
